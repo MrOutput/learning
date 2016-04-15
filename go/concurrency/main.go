d10ed9c64904089342c3269186c1e443e651eb50
brentongillis <bgillisb@gmail.com>
2016-04-15 14:15:49 -0500

    added silly little goroutines

---
+++ go/concurrency/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 0; i < 10000; i++ {
+		wg.Add(1)
+		go func() {
+			for i := 1; i < 1000; i *= 2 {
+				fmt.Printf("%d\n", i)
+			}
+			wg.Done()
+			fmt.Println("Goroutine done...")
+		}()
+	}
+	wg.Wait()
+	fmt.Println("All finished, main exiting...")
+}
