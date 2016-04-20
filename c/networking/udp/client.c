58ae119a80b5f4cc399280c08876f902c72eaab6
Rafael Cepeda <rcepeda1993@gmail.com>
2016-04-20 16:23:52 -0500

    wip

--- c/networking/udp/client.c
+++ c/networking/udp/client.c
@@ -5,8 +5,6 @@
 #include <netinet/udp.h>
 #include <arpa/inet.h>
 
-#include <unistd.h>
-
 int main(int argc, char *argv[]) {
     int udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
     printf("Socket made: %d\n", udp_socket);
@@ -14,7 +12,7 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in address;
     
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = inet_addr("192.168.0.23");
+    address.sin_addr.s_addr = inet_addr("127.0.0.1");
     address.sin_port = htons(3333);
 
     int connection = connect(udp_socket, (struct sockaddr *) &address, sizeof address);
@@ -24,8 +22,6 @@ int main(int argc, char *argv[]) {
         printf("Sending message: %s\n", message);
 
         send(udp_socket, (void *) message, sizeof message, MSG_WAITALL);
-
-        sleep(10);
         shutdown(udp_socket, 2);
     } else {
         return 1;
