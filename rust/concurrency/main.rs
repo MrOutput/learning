eb634cd3c1c55f62d1a023b5e97efb1db8194b34
Rafael Cepeda <rcepeda1993@gmail.com>
2016-05-11 17:15:51 -0500

    rs concur

--- rust/concurrency/main.rs
+++ rust/concurrency/main.rs
@@ -1,12 +1,19 @@
-use std::thread;
-
 fn main() {
-    let child = thread::spawn(move || {
-        for i in 1..1000 {
-            println!("{}", i);
-        }
-        println!("Thread finished.");
-    });
+    let mut threads = Vec::new();
 
-    let result = child.join();
+    for i in 0..10 {
+        let t = std::thread::spawn(move || {
+            for j in 0..10 {
+                if j % 2 == 0 {
+                    println!("From {}: {}", i, j);
+                }
+            }
+        });
+
+        threads.push(t);
+    }
+
+    for t in threads {
+        t.join().unwrap();
+    }
 }
