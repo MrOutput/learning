5a70e9b848aa299d9ab2d09c0a7f39f65601e126
Rafael Cepeda <rcepeda1993@gmail.com>
2016-06-07 17:14:01 -0500

    effective addressing

--- asm/args.asm
+++ asm/args.asm
@@ -1,8 +1,8 @@
-; prints argc
+;prints argc
 
 SECTION .data
-buf: DB ` \n`
-BUF_LEN		EQU 2
+buf db ` \n`
+; nothing here yet
 
 SECTION .text
 GLOBAL _start
@@ -13,16 +13,34 @@ EXIT_SUCCESS	EQU 0
 STDOUT		EQU 1
 ITOA_OFFSET	EQU '0'
 
-_start: mov rax, [rsp]
-	mov byte [buf], al ;move argc into buffer
+_start:	mov rax, [rsp]	;move argc
+	mov rbp, rsp		;new stack frame
+
+	push rax		;int arg for itoa
+	call itoa
+	add rsp, 8		;clean arg off stack
+
+	mov rdx, [rax]		;save char len in rdx for printing
+	inc rax			;increment addres to start of char array
+	mov rsi, rax		;move char address into rsi for prnting
 
 print:	mov rax, SYS_WRITE
 	mov rdi, STDOUT
-	mov rsi, buf
-	add qword [rsi], ITOA_OFFSET
-	mov rdx, BUF_LEN
 	syscall
 
 exit:	mov rax, SYS_EXIT
 	mov rdi, EXIT_SUCCESS
 	syscall
+
+;char* itoa(int)
+;return a pointer to chars
+;stores length of chars in first byte
+itoa:	push rbp
+	mov rbp, rsp		; new stack frame
+
+	mov rax, [rbp + 2 * 8]	;int arg into rax
+	add rax, ITOA_OFFSET
+
+	mov rsp, rbp
+	pop rbp
+	ret
