3ca9bf54b9a52030a10d01035db0a79620513d66
Rafael Cepeda <rcepeda1993@gmail.com>
2016-09-25 17:37:57 -0500

    http client

--- c/networking/http_client.c
+++ c/networking/http_client.c
@@ -1,31 +1,24 @@
 #include <stdio.h>
-
-#include <sys/socket.h>
 #include <netdb.h>
-
 #include <unistd.h>
 
 int main(void)
 {
     int client = socket(AF_INET, SOCK_STREAM, 0);
-    struct addrinfo *res;
-    struct addrinfo hints;
-    hints.ai_protocol = 0;
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
 
-    getaddrinfo("rlc.ninja", "http", &hints, &res);
+    struct addrinfo *res;
+    getaddrinfo("rlc.ninja", "http", NULL, &res);
 
     int conn = connect(client, res->ai_addr, sizeof *res->ai_addr);
-    char buf[] = "GET / HTTP/1.1\n"
+    char req[] = "GET / HTTP/1.1\n"
                  "Host: www.rlc.ninja\n"
                  "Content-Length: 0\n"
                  "\n";
-    send(client, (void *) buf, sizeof buf, 0);
+    write(client, req, sizeof req);
 
-    char recvbuf[512];
-    while (read(client, (void *) &recvbuf, sizeof recvbuf)) {
-        printf("%s\n", recvbuf);
+    char resp[100];
+    while (read(client, &resp, sizeof resp)) {
+        printf("%s", resp);
     }
 
     close(client);
