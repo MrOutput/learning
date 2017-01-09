d285120ca587d24c124857dfde157e2b37f9dd67
Rafael Cepeda <rcepeda1993@gmail.com>
2017-01-09 01:16:17 -0600

    make buf struct

--- c/shellv2.c
+++ c/shellv2.c
@@ -2,27 +2,32 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-enum { BUFINIT = 16 };
 int bufi;
-int buflen;
-char *buf;
+struct buf {
+	int len;
+	char *val;
+};
+struct buf inbuf;
+
+void chgbuf(struct buf *b, int len)
+{
+	b->len = len;
+	b->val = realloc(b->val, len);
+}
 
 void bufc(char c)
 {
-	if (bufi == buflen-1) {
-		buflen *= 2;
-		buf = realloc(buf, buflen);
-	}
-	buf[bufi] = toupper(c);
-	buf[++bufi] = '\0';
+	if (bufi == inbuf.len-1)
+		chgbuf(&inbuf, inbuf.len*2);
+	inbuf.val[bufi] = toupper(c);
+	inbuf.val[++bufi] = '\0';
 }
 
 void bufrst(void)
 {
-	if (buflen != BUFINIT) {
-		buflen = BUFINIT;
-		buf = realloc(buf, buflen);
-	}
+	int init = 16;
+	if (inbuf.len != init)
+		chgbuf(&inbuf, init);
 	bufi = 0;
 }
 
@@ -37,7 +42,7 @@ prompt:
 	while ((c = getchar()) != EOF) {
 		if (c == '\n') {
 			if (bufi > 0)
-				puts(buf);
+				puts(inbuf.val);
 			goto prompt;
 		}
 		bufc(c);
