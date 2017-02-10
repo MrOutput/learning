48db1d10b4084752b7793ae6025767997cea910c
Rafael Cepeda <rcepeda1993@gmail.com>
2017-02-10 14:14:53 -0600

    std

--- c/shellv2.c
+++ c/shellv2.c
@@ -1,73 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
 
 #define HIST_SZ 5
-
-struct dynbuf {
-	int len;
-	int i;
-	char *buf;
-} input;
-
-static void chgbuf(struct dynbuf *b, int len)
-{
-	b->len = len;
-	b->buf = realloc(b->buf, len);
-}
-
-static void bufc(char c)
-{
-	if (input.i == input.len-1)
-		chgbuf(&input, input.len*2);
-	input.buf[input.i++] = c;
-}
-
-static void bufrst(void)
-{
-	int init = 16;
-	if (input.len != init)
-		chgbuf(&input, (init));
-	input.i = 0;
-}
 
 struct {
 	int n;
 	char *buf[HIST_SZ];
 } hist;
 
-static void addhist()
+static void addhist(char *s)
 {
 	if (hist.n < HIST_SZ)
 		hist.n++;
 	else
 		free(hist.buf[HIST_SZ-1]);
 	memmove(hist.buf+1, hist.buf, (sizeof(char *) * (HIST_SZ-1)));
-	hist.buf[0] = strdup(input.buf);
+	hist.buf[0] = strdup(s);
 }
 
 int main(int argc, char *argv[])
 {
-	int c;
-
-prompt:
-	bufrst();
-	printf("> ");
-
-	while ((c = getchar()) != EOF) {
-		switch (c) {
-		case '\n':
-			if (input.i > 0) {
-				bufc('\0');
-				addhist();
-			}
-			goto prompt;
-		default:
-			bufc(c);
-		}
+	char *input = NULL;
+	size_t len = 0;
+	for (;;) {
+		printf("> ");
+		getline(&input, &len, stdin);
+		addhist(input);
 	}
-
-	printf("\n");
 	return 0;
 }
