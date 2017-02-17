2709bbf15b2897598142380c1b44734e810a6a1d
Rafael Cepeda <rcepeda1993@gmail.com>
2017-02-16 21:50:44 -0600

    angular2

--- js/test/js/ui.js
+++
@@ -1,8 +0,0 @@
-function add_class(el, classname) {
-    el.className += " " + classname;
-}
-
-function remove_class(el, classname) {
-    el.className = el.className.replace(classname, "");
-    el.className = el.className.trim();
-}
