4a7701ed8c2d125ba7300c797f44af5a717c91b2
Rafael Cepeda <rcepeda1993@gmail.com>
2017-07-19 23:49:54 -0500

    improve func

--- asm/args_libc.asm
+++ asm/args_libc.asm
@@ -1,27 +1,29 @@
+; BUILD WITH
+;   nasm -f elf64 main.asm
+;   ld -s main.o -lc -dynamic-linker /lib64/ld-linux-x86-64.so.2
+;
 section .data
-	format: db `%d: %s\n\0`
+	fmt: db `%d: %s\n\0`
 
 section .text
 global _start
 extern printf, exit
 
 _start:
-	mov rbp, rsp
-	add rbp, 8
+	mov r13, [rsp] ;argc
+	lea rbx, [rsp + 8]   ;add dword for int, and dword for pad (8-byte alignment)
 
-	mov r12, 0         ;index
-	mov r13, [rbp - 8] ;argc
-	
-.loop:
-	mov rdi, format
+	mov r12, 0     ;index
+; do-while loop
+loop:
+	mov rdi, fmt   ; set fmt str
+	inc r12        ; i+1
 	mov rsi, r12
-	mov rdx, [rbp + r12 * 8]
+	mov rdx, [rbx]
 	call printf
+	add rbx, 8     ; point to next el
+	cmp r12, r13   ; loop condit
+	jl loop
 
-	inc r12
-	cmp r12, r13
-	jl .loop
-
-.done:
-	mov rdi, 0
+	xor rdi, rdi
 	call exit
